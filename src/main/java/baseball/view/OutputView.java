71fd11e89e8cb69fdcd3a3575d8fd1363706aeb1
선민정 (Cloud SW개발팀) <minjung.sun@kt.com>
2022-04-17 15:22:10 +0900

    feat : 출력 메세지

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class OutputView {
+
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String CORRECT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NOTHING = "낫싱";
+
+
+}
