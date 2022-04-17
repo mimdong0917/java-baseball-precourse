1fb4737cc747005475125046546649a390670348
선민정 (Cloud SW개발팀) <minjung.sun@kt.com>
2022-04-17 17:45:36 +0900

    feat : 숫자 야구 게임 실행 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,57 @@
 package baseball;
 
+import baseball.domain.BaseballRandomValGenerator;
+import baseball.domain.UserVal;
+import baseball.service.BaseballGame;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        do {
+            List<Integer> baseballRandomValList = new BaseballRandomValGenerator().getBaseballRandomValList();
+            newUserVal(baseballRandomValList);
+        } while(isNewGame());
+
     }
+
+    private static void newUserVal(List<Integer> baseballRandomValList) {
+        List<Integer> userValList = new UserVal().getUserValList();
+
+        BaseballGame baseballGame = new BaseballGame(baseballRandomValList, userValList);
+
+        if(!baseballGame.isWinning()) {
+            printResult(baseballGame);
+            newUserVal(baseballRandomValList);
+            return;
+        }
+
+        OutputView.printWinningMessage();
+
+    }
+
+    private static boolean isNewGame() {
+        int NEW_GAME = 1;
+        int QUIT = 2;
+
+        int userValNeworQuit = InputView.getNewGameOrEndVal();
+
+        if(userValNeworQuit != NEW_GAME && userValNeworQuit != QUIT) {
+            throw new IllegalArgumentException("입력 오류");
+        }
+
+        return userValNeworQuit == NEW_GAME;
+    }
+
+    private static void printResult(BaseballGame baseballGame) {
+        if(baseballGame.isNothing()) {
+            OutputView.printNothingMessage();
+            return;
+        }
+        OutputView.printStrikeAndBallMessage(baseballGame.getStrikeNumber(), baseballGame.getBallNumber());
+    }
+
 }
